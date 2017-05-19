be22e16afbd959c42853d1ce8becb4ba460139ac
Avni Ghael <avni@Avnis-MacBook-Pro.local>
2017-05-19 16:18:19 -0400

    Changed app.js to fix app error on heroku

--- test/app.js
+++ test/app.js
@@ -9,7 +9,7 @@ module.exports = function(app)
         var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
         var password = process.env.MLAB_PASSWORD_WEBDEV;
         connectionString = 'mongodb://' + username + ':' + password;
-        connectionString += 'ds011775.mlab.com:11775/heroku_4l5x0w1r'; // user yours
+        connectionString += '@ds011775.mlab.com:11775/heroku_4l5x0w1r'; // user yours
     }
 
     var mongoose = require("mongoose");
