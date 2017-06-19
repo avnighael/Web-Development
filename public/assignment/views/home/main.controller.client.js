f5dcb20d76a67cd9d06a1514157ae9036c966020
Avni Ghael <avni@Avnis-MacBook-Pro.local>
2017-06-18 21:38:06 -0400

    Fixed logout on home page

--- public/assignment/views/home/main.controller.client.js
+++ public/assignment/views/home/main.controller.client.js
@@ -3,10 +3,19 @@
         .module('WAM')
         .controller('mainController', mainController);
 
-    function mainController(currentUser) {
+    function mainController(currentUser, userService, $location) {
         var model = this;
 
         model.currentUser = currentUser;
+        model.logout = logout;
+
+        function logout(currentUser){
+            userService
+                .logout()
+                .then(function () {
+                    $location.url('/');
+                })
+        }
 
     }
 
